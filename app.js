14eef0f061584c129c2b892ce74199f86fd8d9c6
182Marco <marcomilza@gmail.com>
2023-01-24 21:24:13 +0100

    validation done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const getNode = str => document.querySelector(str)
+const getNodes = str => document.querySelectorAll(str)
 
 // constructors
 function Book(title, author, isbn) {
@@ -10,11 +11,12 @@ function Book(title, author, isbn) {
 function UI() {}
 
 UI.prototype.addBookToList = function (b) {
-  getNode('#book-list').innerHTML = `<tr>
+  getNode(`#book-list`).innerHTML += 
+  `<tr>
       <td>${b.title}</td>
       <td>${b.author}</td>
       <td>${b.isbn}</td>
-      <td><a href="#" class="delete">X</a></td>
+      <td class="delete">X</td>
    </tr>`
 }
 
@@ -25,18 +27,44 @@ UI.prototype.clearFields = function () {
   }
 }
 
+
+UI.prototype.showAlert = function (msg, className) {
+  getNode(`section`).innerHTML = 
+  `<div class="${className} alert">
+      <td>${msg}</td>
+   </div>`
+   setTimeout(
+     () => getNode(`.alert`).remove(),
+     3000
+   )
+}
+
+UI.prototype.deleteBook = function ({target}) {
+   if(target.className == 'delete') {
+     target.parentElement.remove()
+   }
+   UI.prototype.showAlert(`deleted`,``)
+}
+
 // fn
 const subForm = e => {
   e.preventDefault()
   const ui = new UI()
-  ui.addBookToList(
-    new Book(
-      getNode('#title').value,
-      getNode('#author').value,
-      getNode('#isbn').value,
-    ),
-  )
+  const t = getNode('#title').value,
+        a = getNode('#author').value,
+        ib = getNode('#isbn').value;
+  if(t === '' || a === '' || ib === ''){
+    ui.showAlert(`Each field it's required`, 'error')
+    return
+  }
+  ui.addBookToList(new Book(t, a, ib))
   ui.clearFields()
 }
 
+const deleteB = e => {
+  const ui = new UI()
+  ui.deleteBook(e)
+}
+
 getNode('form').addEventListener('submit', e => subForm(e))
+getNode(`#book-list`).addEventListener('click', e => deleteB(e))
